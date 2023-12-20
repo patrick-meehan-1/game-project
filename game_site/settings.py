8e0658f1b5774fb292937ef503b95d7fb71df524
Patrick Meehan <pjmeehanpj33@gmail.com>
2023-12-20 16:20:17 +0000

    added stying to seasons page and got the list of players with links to profiles. will add hover styling to all pages soon and links. Will start working on styling the profile pages next!

--- game_site/settings.py
+++ game_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party Apps
+    'imagekit',
     'players',
     'games',
 ]
--- players/models.py
+++ players/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
 
 # Create your models here.
 class CustomUser(AbstractUser):
     picture = models.ImageField(upload_to='profile_picture/', blank=True)
+    picture_thumbnail = ImageSpecField(source='picture',
+                                       processors=[ResizeToFill(200,200)],
+                                       format='PNG',
+                                       options={'quality':100})
     country = models.CharField(max_length=255, null=True, blank=True)
     points = models.PositiveBigIntegerField(null=True, blank=True)
 
