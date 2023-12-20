737bbe02d918ff7a8467da897fc9f01489ff2c90
Patrick Meehan <pjmeehanpj33@gmail.com>
2023-12-20 17:17:02 +0000

    profile page not working, only shows original user stuff. Might delete profile stuff and just have the user details page for each person?

--- players/views.py
+++ players/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView, UpdateView, DetailView
 from .models import CustomUser, Profile
 
@@ -10,3 +10,10 @@ def home_page(request):
 class ProfilePageView(DetailView):
     model = Profile
     template_name = 'profile.html'
+    context_object_name = 'profile'  # Set the context object name to 'profile'
+
+    
+    def get_object(self, queryset=None):
+        # Use the user ID from the URL parameter to get the correct profile
+        user_id = self.kwargs.get('pk')
+        return get_object_or_404(Profile, player__id=user_id)
